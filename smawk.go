8d871a48044bc929a84ce5704ca230e5b9067147
bmatt468 <bnmtthews@gmail.com>
2016-07-22 14:05:10 -0400

    rough controller outline

--- smawk.go
+++ smawk.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "github.com/SMAWK/smawk-bot"
+    "net/http"
 )
 
 const (
@@ -9,6 +10,7 @@ const (
     bot_url         = "https://mysimplethings.xyz:8443"
     bot_url_token   = "/309LKj2390gklj1LJF2"
     bot_cert        = "smawk_cert.pem"
+    bot_key         = "smawk_key.pem"
 )
 
 func main() {
@@ -20,18 +22,11 @@ func main() {
 
     // Start listening on our webhook for the commands
     // Spin off a goroutine to handle listening elsewhere
-    /*updates := bot.ListenForWebhook("/309LKj2390gklj1LJF2")
-    go http.ListenAndServeTLS("0.0.0.0:8443", "smawk_cert.pem", "smawk_key.pem", nil)
+    updates := bot.Listen(bot_url_token)
+    go http.ListenAndServeTLS("0.0.0.0:8443", bot_cert, bot_key, nil)
 
     // Parse and execute each of the commands that come through the pipe
     for update := range updates {
-        cmd := update.Message.Text
-        if (cmd == "/start" || cmd == "/start@smawk_bot") {
-            msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Lo, the official SMÄWKBot rises!")
-            bot.Send(msg)
-        } else if (cmd == "/hello" || cmd == "/hello@smawk_bot") {
-            msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hello, @" + update.Message.From.UserName + "!")
-            bot.Send(msg)
-        }
-    }*/
+        bot.ParseAndExecuteUpdate(update)
+    }
 }
